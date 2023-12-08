7863ea933b377c7bcdcd519f42f2343c52e724cb
temty180447 <tgtv2547@gmail.com>
2023-12-08 16:23:36 +0700

    XD

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,31 @@
-//Dialog of the first example is given below.
-
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    int id;
+    string movie;
+    string day;
+    int gear;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    getline(cin,name);
+    cout << "?????: ";
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cin >> id;
+    gear = id/10000000 -12;
+    cout << name << ": ";
+    cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you." << endl;
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+    cin.ignore();
+    getline(cin,movie);
+    cout <<name<<": ";
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+    getline(cin,day);
+    cout << name << ": ";
+    cout << "Fahsai: " << day <<"....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
+    cout << name << ": ";
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+return 0;
+}
